6c574a2c4e278964945d5e2d00cc3f55f7f0d22a
Markel Sudnev <m.sudnev@gmail.com>
2022-01-31 21:10:12 +0100

    initial commit

---
+++ src/main/java/by/flathumor/serviceworkshop/ServiceWorkshopApplication.java
@@ -0,0 +1,13 @@
+package by.flathumor.serviceworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/flathumor/serviceworkshop/ServiceWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flathumor.serviceworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
